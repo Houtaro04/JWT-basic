c02f13f6d2db6620ae70a850c56fd00be156de83
Houtaro <cungvietanh04@gmail.com>
2025-08-22 13:57:56 +0700

    adding delete user

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -10,18 +10,13 @@ const userController = {
         }
     },
     deleteUser: async (req, res) => {
-        const userId = req.params.id;
         try {
-            const user = await User.findById(userId);
-            if (!user) {
-                return res.status(404).json({ message: "User not found" });
-            }
-            res.status(200).json({ message: "User deleted successfully" });
-        } catch (error) {
-            console.error(error);
-            res.status(500).json({ message: "Internal server error" });
+            const user = await User.findByIdAndDelete(req.params.id);
+            res.status(200).json("Delete successfully!");
+        }catch(err){
+            res.status(500).json(err);
         }
-    }, 
+    },
 }
 
 module.exports = userController;
