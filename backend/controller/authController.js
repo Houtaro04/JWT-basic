12cf282b357cb00545b79e30be7fd89cc94f47f9
Houtaro <cungvietanh04@gmail.com>
2025-08-25 16:48:30 +0700

    Adding router and controller for refresh Token and Log out

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -2,6 +2,8 @@ const User = require("../models/user");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
+let refreshTokens = [];
+
 const authController = {
     //REGISTER
     registerUser: async(req, res) => {
@@ -23,6 +25,17 @@ const authController = {
             res.status(500).json(err);
         }
     },
+    
+    //GENERATE ACCESS TOKEN
+    generateAccessToken: (user) => {
+        return jwt.sign({ id: user._id, username: user.username, admin: user.admin }, process.env.JWT_SECRET || "dev_secret", { expiresIn: "30s" });
+    },
+
+    //GENERATE REFRESH TOKEN
+    generateRefreshToken: (user) => {
+        return jwt.sign({ id: user._id, username: user.username, admin: user.admin }, process.env.JWT_REFRESH_SECRET || "dev_refresh_secret", { expiresIn: "365d" });
+    },
+
     //LOGIN
     loginUser: async (req, res) => {
         try {
@@ -39,17 +52,57 @@ const authController = {
         const validPassword = await bcrypt.compare(password, user.password);
         if (!validPassword) return res.status(401).json({ message: "Sai mật khẩu" });
 
-        const payload = { id: user._id, username: user.username, admin: user.admin };
-        const token = jwt.sign(payload, process.env.JWT_SECRET || "dev_secret", { expiresIn: "7d" });
-
+        const token = authController.generateAccessToken(user);
+        const refreshToken = authController.generateRefreshToken(user);
+        refreshTokens.push(refreshToken);
+        res.cookie("refreshToken", refreshToken, { 
+            httpOnly: true,
+            secure: false,
+            path: "/",
+            sameSite: "strict"
+        });
         const { password: _pw, __v, ...safe } = user.toObject();
         // Trả token trong body (hoặc set cookie nếu muốn)
-        return res.json({ user: safe, token });
+        return res.json({ user: safe, token, refreshToken });
         } catch (err) {
         console.error(err);
         return res.status(500).json({ message: "Server error" });
         }
     },
-}
+
+    requestRefreshToken: async (req, res) => {
+        //Lấy token từ user
+        const refreshToken = req.cookies.refreshToken;
+        if (!refreshToken) return res.status(401).json("You're not authenticated");
+        if (!refreshTokens.includes(refreshToken)) {
+            return res.status(403).json("Refresh token is not valid");
+        }
+        jwt.verify(refreshToken, process.env.JWT_REFRESH_SECRET || "dev_refresh_secret", (err, user) => {
+            if (err) {
+                console.log(err);
+                return res.status(403).json("Refresh token is not valid");
+            }
+            refreshTokens = refreshTokens.filter(token => token !== refreshToken);
+            //Create new access token, refresh token and send to user
+            const newAccessToken = authController.generateAccessToken(user);
+            const newRefreshToken = authController.generateRefreshToken(user);
+            refreshTokens.push(newRefreshToken);
+            res.cookie("refreshToken", newRefreshToken, {
+                httpOnly: true,
+                secure: false,
+                path: "/",
+                sameSite: "strict"
+            });
+            res.status(200).json({ token: newAccessToken });
+        });
+    },
+
+    //LOG OUT
+    userLogout: async (req, res) => {
+        res.clearCookie("refreshToken");
+        refreshTokens = refreshTokens.filter(token => token !== req.cookies.refreshToken);
+        res.status(200).json({ message: "Logged out!" });
+    }
+};
 
 module.exports = authController;
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -10,4 +10,10 @@ router.post("/register", authController.registerUser);
 //LOGIN
 router.post("/login", authController.loginUser);
 
+//REFRESH
+router.post("/refresh", authController.requestRefreshToken);
+
+//LOGOUT
+router.post("/logout", authController.userLogout);
+
 module.exports = router;
