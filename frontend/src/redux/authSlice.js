8562655e4e36a514d86453791280a2f4793ae2b3
Houtaro <cungvietanh04@gmail.com>
2025-08-29 12:05:21 +0700

    Using Redux toolkit for action login

---
+++ frontend/src/redux/authSlice.js
@@ -0,0 +1,26 @@
+import {creadSlice, current} from "@reduxjs/toolkit";
+
+const authSlice = createSlice({
+    name: 'auth',
+    initialState: {
+        login:{
+            currentUser: null,
+            isFetching: false,
+            error: false,
+        }
+    },
+    reducersL: {
+        loginStart: (state) => {
+            state.login.isFetching = true;
+        },
+        loginSuccess: (state, action) => {
+            state.login.isFetching = false;
+            state.login.currentUser = action.payload;
+            state.login.error = false;
+        },
+        loginFail: (state, action) => {
+            state.login.isFetching = false;
+            state.login.error = true;
+        }
+    }
+})
